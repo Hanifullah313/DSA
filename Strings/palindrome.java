853eabfd880595daa16765f89cf0dde8185af303
hanifullah313 <hu4206835@gmail.com>
2025-04-08 07:41:02 -0700

    palindrome problem soloved 1

---
+++ Strings/palindrome.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class palindrome {
+    static public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            right--;
+            left++;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "A man, a plan, a canal: Panama";
+        String s2 = "race a car";
+        String s3 = " ";
+        System.out.println(isPalindrome(s1));
+        System.out.println(isPalindrome(s2));
+        System.out.println(isPalindrome(s3));
+    }
+}
