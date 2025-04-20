47c4b2928fff99b975a88ea9e02ed8d99fc0794c
hanifullah313 <hu4206835@gmail.com>
2025-04-20 07:51:59 -0700

    Sub Sequence of string

---
+++ Recursion/subsequnceinString.java
@@ -0,0 +1,19 @@
+public class subsequnceinString {
+
+    public static void subsequence(String str, int index, String newstring) {
+        if (index == str.length()) {
+            System.out.println(newstring);
+            return;
+        }
+        char currentchar = str.charAt(index);
+        // to be
+        subsequence(str, index + 1, newstring + currentchar);
+        // or not to be
+        subsequence(str, index + 1, newstring);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        subsequence(str, 0, "");
+    }
+}
