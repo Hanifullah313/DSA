266129352be9cfda5a2ec6f2c14ec3d9d6b10672
hanifullah313 <hu4206835@gmail.com>
2025-05-25 03:49:21 -0700

    Daimter Of Tre

---
+++ Tree/Binary Tree/DaimeterOfTree.java
@@ -0,0 +1,52 @@
+public class DaimeterOfTree{
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node BuildTree(int[] nodes) {
+            idx++;
+            if (idx >= nodes.length || nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static int SumOFNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHieght = SumOFNodes(root.left);
+        int rightHieght = SumOFNodes(root.right);
+        int myheight = Math.max(leftHieght, rightHieght)+1;
+        return myheight;
+    }
+    public static int DaimeterTree(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int Daim1 = DaimeterTree(root.left);
+        int Daim2 = DaimeterTree(root.right);
+        int Daim3 = SumOFNodes(root.left)+SumOFNodes(root.right);
+        return Math.max( Daim3 , Math.max(Daim1, Daim2));
+    }
+
+    public static void main(String[] args) {
+        int[] node = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = BinaryTree.BuildTree(node);
+        System.out.println(DaimeterTree(root));
+    }
+}
